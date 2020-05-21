55c1b26e72fce4638edd7bd05cb4aceeebb06bb9
Максим Бельфер <maksimbelfer@MacBook-Pro-Maksim.local>
2020-05-22 00:55:58 +0300

    initial commit

---
+++ src/components/toDoItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wrapper">
+      <div class="name">{{name}}</div>
+      <span :class="['mark', marked ? 'yel' : '']" @click="$emit('close')"><i class="fa fa-star"></i>Важная</span>
+      <span class="done" @click="$emit('done')"><i class="fa fa-check"></i>Выполнено</span>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'toDoItem',
+    props: {
+        name: {
+            type: String
+        },
+        marked: {
+            type: Boolean,
+            default: false
+        },
+        done: {
+            type: Boolean,
+            default: false
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .wrapper{
+        display: flex;
+        justify-content: space-between;
+        .name{
+            width: 70%;
+            text-align: left;
+        }
+        .mark{
+            margin-right: 10px;
+            .fa{
+                margin-right: 20px;
+            }
+            &.yel{
+                color: orange;
+            }
+        }
+        span{
+            display: flex;
+            font-size: 26px;
+            cursor: pointer;
+        }
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="home">
+    <div class="wrap"><input type="text" v-model="state.current"></div>
+    <todo-item
+      v-for="item in state.todos"
+      :key="item.name"
+      :name="item.name"
+      :marked="item.marked"
+      :done="item.done"
+      :class="item.done ?'done' : ''"
+      @close="item.marked = !item.marked"
+      @done="item.done = !item.done"
+      />
+    <button @click="addOne()" class="add">Добавить</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import toDoItem from '@/components/toDoItem'
+
+export default {
+  name: 'Home',
+  data(){
+    return{
+      state: {
+        todos: [
+          {id:1, name: 'Погладить', marked: false, done:false},
+          {id:2, name: 'Постирать', marked: false, done:false},
+          {id:3, name: 'Кино', marked: false, done:false},
+          {id:4, name: 'Звонок', marked: true, done:true},
+          {id:5, name: 'Диплом', marked: false, done:false},
+        ],
+        current: ''
+      }
+    }
+  },
+  components:{
+    'todo-item' : toDoItem
+  },
+  methods: {
+    addOne: function(){
+      const letter = this.state.current
+      this.state.todos.push({id:25, name: letter, marked:false, done: false})
+      this.state.current = ''
+    },
+    // close: function(e){
+    //   console.log(e)
+    // }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home{
+  width: 900px;
+  margin: 0 auto;
+  font-size: 26px;
+  padding: 20px 10px;
+  border: 1px solid #000;
+  .wrap{
+    input{
+      width: 100%;
+      height: 50px;
+    }
+  }
+  .item{
+    text-align: left;
+    margin-bottom: 10px;
+    padding: 10px;
+  }
+  .add{
+    height: 50px;
+    border: 1px solid #000;
+    border-radius: 25px;
+    font-size: 26px;
+  }
+  .done{
+    text-decoration: line-through;
+  }
+}
+</style>
